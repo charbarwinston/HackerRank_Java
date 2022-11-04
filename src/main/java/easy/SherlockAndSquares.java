97080e6eedab3884a205f08b6f5ff85f4dd420bc
Charles Neff <charlesneff13@gmail.com>
2022-11-04 00:46:12 -0500

    [refactor] Optimized code and handled edge cases

--- src/main/java/easy/SherlockAndSquares.java
+++ src/main/java/easy/SherlockAndSquares.java
@@ -4,13 +4,25 @@ public class SherlockAndSquares {
 
     // Assumes that "a" is always less than "b"
     public int findSquares(int a, int b) {
-        int range = b - a;
         int total = 0;
-        for (int x = 0; x <= range; x++) {
-            if (Math.sqrt(a) % 1 == 0 ) {
-                total++;
+        int start = 0;
+        if (a - b == 0) {
+            if (Math.sqrt(a) % 1 == 0) {
+                return 1;
+            }
+        }
+        while (a < b && start == 0) {
+            if (Math.sqrt(a) % 1 == 0) {
+                start = (int) Math.sqrt(a);
             }
             a++;
+        }
+        //Once you found the first square, you can keep incrementally squaring the root of the starting int until the square is higher than b
+        if (start != 0) {
+            while(start*start <= b) {
+                start++;
+                total++;
+            }
         }
         return total;
     }
--- src/test/java/easy/SherlockAndSquaresTest.java
+++ src/test/java/easy/SherlockAndSquaresTest.java
@@ -11,6 +11,8 @@ public class SherlockAndSquaresTest {
     @Test
     public void should_Determine_Number_Of_Squares_Within_Range() {
         assertEquals(3, solver.findSquares(24, 49));
+        assertEquals(9855, solver.findSquares(465868129 , 988379794));
+        assertEquals(1, solver.findSquares(4 , 4));
     }
 
 }
